8c3f8294d51abc869681fb8db4e5e871d1237cc3
choiseungchul <tmdcjf152@naver.com>
2022-10-07 19:05:50 +0900

    [Add] Nav바 추가 및 레이아웃 설정

--- src/pages/GraphScreen.jsx
+++ src/pages/GraphScreen.jsx
@@ -1,12 +1,47 @@
 import styled from 'styled-components';
+import Nav from '../components/nav/Nav';
+import { blue, pearl } from '../theme';
 
 const GraphScreen = () => {
   return (
-    <StyledGraphScreen>
-      <span>GraphScreen</span>
-    </StyledGraphScreen>
+    <>
+      <StyledGraphScreen>
+        <div className='graph-inner-box'>
+          <span>GraphScreend</span>
+        </div>
+      </StyledGraphScreen>
+    </>
   );
 };
 export default GraphScreen;
 
-const StyledGraphScreen = styled.div``;
+const StyledGraphScreen = styled.div`
+  width: 80%;
+  height: 100vh;
+  margin-left: 20%;
+  padding: 10px;
+  .graph-inner-box {
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    border: 3px solid ${blue};
+  }
+
+  /* ============= ======= 1024px ============ ======== */
+  @media screen and (max-width: 1024px) {
+  }
+
+  /* ============= ======= 890px ============ ======== */
+  @media screen and (max-width: 890px) {
+    margin-left: 0;
+    margin-top: 250px;
+  }
+
+  /* ============= ======= 480px ============ ======== */
+  @media screen and (max-width: 480px) {
+  }
+
+  /* ============= ======= 378px ============ ======== */
+  @media screen and (max-width: 378px) {
+  }
+`;
--- src/pages/SensorList.jsx
+++ src/pages/SensorList.jsx
@@ -1,15 +1,40 @@
 import styled from 'styled-components';
-import { blue } from '../theme';
+import { blue, pearl } from '../theme';
 
 const SensorList = () => {
   return (
     <StyledSensor>
-      <span>SensorList</span>
+      <span  >SensorList</span>
     </StyledSensor>
   );
 };
 export default SensorList;
 
 const StyledSensor = styled.div`
+  width: 80%;
+  height: 100vh;
+  margin-left: 20%;
+  padding: 10px;
   color: ${blue};
+
+
+
+
+  /* ============= ======= 1024px ============ ======== */
+  @media screen and (max-width: 1024px) {
+  }
+
+  /* ============= ======= 890px ============ ======== */
+  @media screen and (max-width: 890px) {
+    margin-left: 0;
+    margin-top: 250px;
+  }
+
+    /* ============= ======= 480px ============ ======== */
+  @media screen and (max-width: 480px) {
+  }
+
+    /* ============= ======= 378px ============ ======== */
+  @media screen and (max-width: 378px) {
+  }
 `;
